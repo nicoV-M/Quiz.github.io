f5cc3ae466fcfe5e61d412066fd603c5316de2b5
leoV0 <leo.cigale@gmail.com>
2025-02-17 15:40:36 +0100

    Ajout des questions

---
+++ questions.js
@@ -0,0 +1,73 @@
+export const quizVoyage = {  
+    questions: [
+        {
+          text: "Dans quel pays se trouve la tour Eiffel rouge et blanche ?",
+          options: [
+            "New-York",
+            "Singapour",
+            "Japon",
+            "Los Angeles"
+          ],
+          correct_answer: "Japon"
+        },
+        {
+          text: "Quel monument est le plus connu à Sydney ?",
+          options: [
+            "St Mary's Cathedral",
+            "L'opéra de Sydney",
+            "Sydney tower eye",
+            "Queen Victoria Building"
+          ],
+          correct_answer: "L'opéra de Sydney"
+        },
+        {
+            text: "Sur quel sommet le christ rédempteur se situe-t-il ?",
+            options:[
+                "Corcovado",
+                "Pedra Bonita",
+                "Pic Caratuva",
+                "Pedra da Mina",
+            ],
+            correct_answer: "Corcovado"
+        },
+        {
+            text:"Quel est le nom du parc en mosaïque en Espagne ?",
+            options:[
+                "Le Parc du Retiro",
+                "Le Parc Güell",
+                "Le Parc de la Ciutadella",
+                "Le Parc del Laberint d’Horta",
+            ],
+            correct_answer: "Le Parc Güell"
+        },
+        {
+            text: "Où le Golden Gate se situe-t-il ?",
+            options:[
+                "Santa Barbara",
+                "San Diego",
+                "San Francisco",
+                "Palm Springs",
+            ],
+            correct_answer: "San Francisco",
+        },
+        {
+            text: "Quel sport est le plus populaire au Japon ?",
+            options:[
+                "Football",
+                "Hockey sur glace",
+                "Baseball",
+                "Basket-ball",
+            ],
+            correct_answer: "Baseball",
+        },
+        {
+            text: "En quelle année la pierre de Rosette a t-elle été découverte ?",
+            options:[
+                "1634",
+                "1910",
+                "1865",
+                "1799",
+            ],
+            correct_answer: "1799",
+        }
+    ]};
