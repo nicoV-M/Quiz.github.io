b39e8937653b405d02f895c228b92eaa17e976ae
leoV0 <leo.cigale@gmail.com>
2025-02-18 16:28:30 +0100

    Bouton rejouer

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ console.log(progressionQuiz)
 const contenuQuestion = document.querySelector('#question-text');
 const contenuOptions = document.querySelector('#options-container');
 const boutonSuivant = document.querySelector('#next-button');
+const restartGame = document.querySelector('#replay-button');
 let i = 0
 
 // Fonction pour afficher une question basée sur l'index actuel
@@ -45,8 +46,20 @@ boutonSuivant.addEventListener('click', () => {
     contenuQuestion.innerText = 'test terminé';
     contenuOptions.innerHTML = ''; // Effacer les options
     boutonSuivant.style.display = 'none'; // Cacher le bouton Suivant
+    restartGame.style.display = 'inline-block'; // Afficher le bouton restartGame
   }
 });
 
 // Charger la première question au chargement de la page
 loadQuestion();
+
+// Fonction pour réinitialiser le quiz
+restartGame.addEventListener('click', () => {
+    // TODO Réinitialiser l'index
+    i = 0;
+    // TODO Cacher le bouton Rejouer et afficher le bouton Suivant
+    nextQuestion.style.display = 'inline-block';
+    restartGame.style.display = 'none'; 
+    // TODO Recharger la première question
+    loadQuestion();
+  });
